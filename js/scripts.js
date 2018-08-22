a2e9cd05543a39cbd6c35f5df76c36750c594067
Ilona Pastucha <ilonka.p4@gmail.com>
2018-08-22 19:51:04 +0200

    Clear code

--- js/scripts.js
+++ js/scripts.js
@@ -7,44 +7,44 @@
   var listItems = '';
   var view = document.getElementById('carousel');
   var elem = document.querySelector('.main-carousel');
-  
+
   Mustache.parse(gallery);
-  
-    for (var i = 0; i < galleryData.length; i++) {
-      listItems += Mustache.render(gallery, galleryData[i]);
-    };
+
+  for (var i = 0; i < galleryData.length; i++) {
+    listItems += Mustache.render(gallery, galleryData[i]);
+  };
   view.innerHTML = listItems;
 
-    // Gallery slide carusel Flickity
-    var elem = document.querySelector('.main-carousel');
-    var flkty = new Flickity(elem, {
-      // options
-      cellAlign: 'left',
-      hash: true,
-      contain: true,
-      freeScroll: true,
-      pageDots: false,
-      groupCells: true
-    });
-    
-
-    // restart button 
-    var button = document.querySelector('.button');
-    
-    button.addEventListener('click', function(event) {
-      flkty.selectCell(0);
-    });
-    
-    // pasek postempu
-    var progressBar = document.querySelector('.progress-bar')
-
-    flkty.on('scroll', function(progress) {
-    progress = Math.max(0, Math.min( 1, progress));
-    progressBar.style.width = progress * 100 + '%';
-    });
-
-    var flkty = new Flickity( '.main-carousel', {
+  // Gallery slide carusel Flickity
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    hash: true,
+    contain: true,
+    freeScroll: true,
+    pageDots: false,
     groupCells: true
-    });
+  });
+
+
+  // restart button 
+  var button = document.querySelector('.button');
+
+  button.addEventListener('click', function (event) {
+    flkty.selectCell(0);
+  });
+
+  // pasek postempu
+  var progressBar = document.querySelector('.progress-bar')
+
+  flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+  });
+
+  var flkty = new Flickity('.main-carousel', {
+    groupCells: true
+  });
 
 })();
